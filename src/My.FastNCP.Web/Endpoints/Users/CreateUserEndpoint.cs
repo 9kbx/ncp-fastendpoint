956dfe37b30252e4d900f0282304c4080ac8fd61
9kbx <550591004@qq.com>
2025-06-27 11:30:10 +0800

    增加创建适合ncp的endpoint代码快捷指令`epp`

--- src/My.FastNCP.Web/Endpoints/Users/CreateUserEndpoint.cs
+++ src/My.FastNCP.Web/Endpoints/Users/CreateUserEndpoint.cs
@@ -4,21 +4,41 @@ using NetCorePal.Extensions.Dto;
 
 namespace My.FastNCP.Web.Endpoints.Users;
 
-public record CreateUserRequest(string Username, string Password);
-
-public class CreateUserEndpoint(IMediator mediator)
-    : Endpoint<CreateUserRequest, ResponseData<CreateUserCommandResponse>>
+sealed class CreateUserEndpoint(IMediator mediator) : Endpoint<CreateUserRequest, ResponseData<CreateUserResponse>>
 {
     public override void Configure()
     {
         Post("/api/user/create");
-        Permissions("user.create");
+        AllowAnonymous();
     }
 
-    public override async Task HandleAsync(CreateUserRequest req, CancellationToken ct)
+    public override async Task HandleAsync(CreateUserRequest r, CancellationToken c)
     {
-        var cmd = new CreateUserCommand(req.Username, req.Password);
-        var res = await mediator.Send(cmd, ct);
-        await SendOkAsync(res.AsResponseData(), ct);
+        var cmd = new CreateUserCommand(r.UserName, r.Password);
+        var result = await mediator.Send(cmd, c);
+        var res = new CreateUserResponse(result.UserId, result.UserName);
+        await SendOkAsync(res.AsResponseData(), c);
+        
+    }
+}
+
+sealed record CreateUserRequest(string UserName, string Password);
+
+sealed record CreateUserResponse(long UserId, string UserName);
+
+sealed class CreateUserValidator : Validator<CreateUserRequest>
+{
+    public CreateUserValidator()
+    {
+        // RuleFor(x => x.Property).NotEmpty();
+    }
+}
+
+sealed class CreateUserSummary : Summary<CreateUserEndpoint, CreateUserRequest>
+{
+    public CreateUserSummary()
+    {
+        Summary = "创建用户";
+        Description = "Description text goes here...";
     }
 }
--- src/My.FastNCP.Web/Endpoints/Users/DeleteUserEndpoint.cs
+++ src/My.FastNCP.Web/Endpoints/Users/DeleteUserEndpoint.cs
@@ -3,22 +3,40 @@ using NetCorePal.Extensions.Dto;
 
 namespace My.FastNCP.Web.Endpoints.Users;
 
-public class DeleteUserRequest
-{
-    [RouteParam] public long UserId { get; set; }
-}
 
-public class DeleteUserEndpoint : Endpoint<DeleteUserRequest, ResponseData>
+sealed class DeleteUserEndpoint : Endpoint<DeleteUserRequest, ResponseData>
 {
     public override void Configure()
     {
         Delete("/api/user/{userId}/delete");
-        // Roles("Admin");
         Permissions("user.delete");
     }
 
-    public override async Task HandleAsync(DeleteUserRequest req, CancellationToken ct)
+    public override async Task HandleAsync(DeleteUserRequest r, CancellationToken c)
     {
-        await SendOkAsync(true.AsResponseData(), ct);
+        await SendOkAsync(true.AsResponseData(), c);
+    }
+}
+
+sealed class DeleteUserRequest
+{
+
+    [RouteParam] public long UserId { get; set; }
+}
+
+sealed class DeleteUserValidator : Validator<DeleteUserRequest>
+{
+    public DeleteUserValidator()
+    {
+
+    }
+}
+
+sealed class DeleteUserSummary : Summary<DeleteUserEndpoint, DeleteUserRequest>
+{
+    public DeleteUserSummary()
+    {
+        Summary = "删除用户";
+        Description = "";
     }
 }
